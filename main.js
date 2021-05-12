221fe041e576a00c7cce787f8625df1b7e16392f
katequillen <kate.quillen@gmail.com>
2021-05-12 11:05:21 +0200

    final push

--- main.js
+++ main.js
@@ -1,48 +1,37 @@
 "use strict";
 
-const leadNumber = document.querySelector(".fill_number");
-const clue = document.querySelector(".fill_clue");
-const attempts = document.querySelector(".fill_attempts")
 const max = 100;
 const generatedNumber = Math.ceil(Math.random() * max);
-const button = document.querySelector(".button_test")
-
-console.log(generatedNumber);
+const button = document.querySelector('.button_test')
+const guessfill= document.getElementById('number');
+const guess = parseInt(guessfill.innerHTML);
+const clue = document.querySelector('.fill_clue');
+const attempts = document.querySelector('.fill_attempts')
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
    }
+console.log(generatedNumber);
 
-if(leadNumber < generatedNumber) {
+//console.log(guess);
+
+
+if (guess.value < generatedNumber) {
     clue.innerHTML = `Your number is too low.`;
-    }
-else if (leadNumber > generatedNumber){
-        clue.innerHTML = `Your number is too high.`;
-    }
-    else if (leadNumber === generatedNumber) {
-        clue.innerHTML = `Congratulations! You guessed correctly.`
-    }
-    else {
-        clue.innerHTML = `Please enter a valid number.`
-    }
-
-// function handleClickButton() {
-//   attempts.innerHTML = ('Numero de intentos:' + click++);
-// }
+} else if (guess.value > generatedNumber) {
+    clue.innerHTML = `Your number is too high.`;
+} else if (guess.value === generatedNumber) {
+    clue.innerHTML = `Congratulations! You guessed correctly.`
+} else if (guess > 100) {
+    clue.innerHTML = `Please enter a valid number.`;
+} else if (guess < 1) {
+    clue.innerHTML = `Please enter a valid number.`;
+}   
 
 
-// for (let button = 0; button < 1000; button++) {
-//     attempts.innerHTML = ('Numero de intentos:' + button);
+
+// for (let i = 0; i < 10; i++) {
+//     attempts.innerHTML = ('Numero de intentos:' + i);
 //   }
        
 // button.addEventListener( 'click' , handleClickButton );
-
-
-   //get random number
-   // lead introduced number
-   //if number too big
-   //if number too small
-   //if number the same
-   //if number not valid
-
-   //number of attempts
